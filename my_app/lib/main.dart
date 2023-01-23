10a976eafc402b8fa50a1430f56d66831223a39e
Akito_macbookpro <akito.kawamura0310@gmail.com>
2023-01-24 00:20:42 +0900

    add navigation

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,75 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/next_page.dart';
+
+import 'src/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
-      home: const MyHomePage(title: 'Test'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      // アクションボタン
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => NextPage()),
-          );
-        },
-        tooltip: 'Increment',
-        backgroundColor: Colors.green,
-        child: const Icon(Icons.accessibility_new_sharp),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
 }
