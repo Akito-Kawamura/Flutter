b9d5a464abb57ae24ccf8cd7f8344411559c39b9
Akito_macbookpro <akito.kawamura0310@gmail.com>
2023-04-19 23:48:57 +0900

    add test

---
+++ stock_mate/test/main_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stock_mate/main.dart';
+
+void main() {
+  testWidgets('Main app test', (WidgetTester tester) async {
+    // MyAppウィジェットをビルドしてテスト
+    await tester.pumpWidget(MyApp());
+
+    // 期待されるタイトルが存在することを確認
+    expect(find.text('家の消費財在庫管理'), findsOneWidget);
+  });
+}
